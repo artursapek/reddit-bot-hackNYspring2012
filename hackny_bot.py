1c583cfbf228694facc63c7e5a5535a972787847
jason <jason@jason-TOSHIBA-NB505.(none)>
2012-03-24 19:50:27 -0400

    markov shit

--- hackny_bot.py
+++ hackny_bot.py
@@ -2,15 +2,15 @@ import reddit
 import pymongo
 
 #419 reddits
-
-SUBS_LIST = ['4chan', 'AbandonedPorn', 'adventuretime', 'AdviceAnimals', 'AlienBlue', 'alternativeart', 'AMA', 'Amateur', 'amiugly', 'Anarchism', 'Android', 'AnimalPorn', 'anime', 'announcements', 'answers', 'Anthropology', 'apple', 'ArcherFX', 'architecture', 'arresteddevelopment', 'Art', 'AsianHotties', 'AskReddit', 'askscience', 'asmr', 'asoiaf', 'ass', 'Astronomy', 'atheism', 'Austin', 'australia', 'Autos', 'aww', 'Bacon', 'Bad_Cop_No_Donut', 'Baking', 'baseball', 'battlestations', 'beer', 'bestof', 'bicycling', 'biology', 'blog', 'Blowjobs', 'boardgames', 'Boobies', 'books', 'boston', 'breakingbad', 'Buddhism', 'buildapc', 'business', 'BuyItForLife', 'calvinandhobbes', 'CampingandHiking', 'canada', 'cannabis', 'carlhprogramming', 'cars', 'catpictures', 'cats', 'Celebs', 'CFB', 'chemistry', 'chicago', 'Christianity', 'chrome', 'circlejerk', 'CityPorn', 'classicalmusic', 'classicrage', 'climbing', 'coding', 'Coffee', 'cogsci', 'collapse', 'comicbooks', 'comics', 'community', 'compsci', 'conspiracy', 'Cooking', 'cordcutters', 'creepy', 'cumsluts', 'cyberlaws', 'DealsReddit', 'Demotivational', 'depression', 'DepthHub', 'Design', 'DesignPorn', 'DestructionPorn', 'Dexter', 'Diablo', 'dirtysmall', 'DIY', 'DnB', 'doctorwho', 'Documentaries', 'DoesAnybodyElse', 'Dogfort', 'dogs', 'DotA2', 'Drugs', 'drunk', 'dubstep', 'dwarffortress', 'EarthPorn', 'Economics', 'economy', 'education', 'electrohouse', 'electronicmusic', 'electronics', 'EmmaWatson', 'energy', 'engineering', 'Enhancement', 'entertainment', 'Entrepreneur', 'environment', 'europe', 'explainlikeimfive', 'facepalm', 'Fantasy', 'fashion', 'Favors', 'feminisms', 'fffffffuuuuuuuuuuuu', 'fffffffuuuuuuuuuuuud', 'fifthworldproblems', 'finance', 'firefly', 'firstworldproblems', 'fitmeals', 'Fitness', 'food', 'Foodforthought', 'FoodPorn', 'ForeverAlone', 'forhire', 'freebies', 'Freethought', 'Frugal', 'frugalmalefashion', 'funny', 'futurama', 'gadgets', 'GameDeals', 'gamedev', 'gameofthrones', 'gamernews', 'Games', 'gaming', 'gardening', 'gaymers', 'geek', 'GeekPorn', 'gentlemanboners', 'GetMotivated', 'gif', 'gifs', 'ginger', 'girlsinyogapants', 'GirlswithGlasses', 'gonewild', 'GoneWildPlus', 'google', 'Graffiti', 'Green', 'Guildwars2', 'Guitar', 'guns', 'happy', 'hardware', 'harrypotter', 'Health', 'HIMYM', 'hiphopheads', 'history', 'HistoryPorn', 'hockey', 'Homebrewing', 'horror', 'Hotchickswithtattoos', 'howto', 'HumanPorn', 'humor', 'IAmA', 'IDAP', 'iiiiiiitttttttttttt', 'IndieGaming', 'investing', 'ipad', 'iphone', 'itookapicture', 'IWantOut', 'IWantToLearn', 'japan', 'javascript', 'Jazz', 'jobs', 'Jokes', 'keto', 'LadyBoners', 'law', 'leagueoflegends', 'learnprogramming', 'lectures', 'LegalTeens', 'lego', 'lgbt', 'Libertarian', 'LifeProTips', 'linguistics', 'linux', 'listentothis', 'lists', 'literature', 'lolcats', 'LosAngeles', 'loseit', 'lost', 'LucidDreaming', 'MachinePorn', 'magicTCG', 'malefashionadvice', 'MapPorn', 'Marijuana', 'mashups', 'masseffect', 'math', 'Meditation', 'meetup', 'MensRights', 'Metal', 'milf', 'Military', 'Minecraft', 'MMA', 'modnews', 'motorcycles', 'moviecritic', 'movies', 'Music', 'mw3', 'mylittlepony', 'nba', 'NetflixBestOf', 'netsec', 'newreddits', 'news', 'nfl', 'nosleep', 'nostalgia', 'nsfw', 'nsfw2', 'nsfw_gifs', 'NSFW_nospam', 'nyc', 'obama', 'occupywallstreet', 'O_Faces', 'offbeat', 'OneY', 'OnOff', 'opendirectories', 'opensource', 'Paleo', 'Paranormal', 'Parenting', 'passionx', 'personalfinance', 'Pets', 'philosophy', 'PhilosophyofScience', 'photography', 'photos', 'PHP', 'Physics', 'pics', 'Pictures', 'pokemon', 'politics', 'pornvids', 'Portal', 'Portland', 'PostCollapse', 'productivity', 'programming', 'progressive', 'ProjectEnrichment', 'ProjectReddit', 'proper', 'PS3', 'psychology', 'Psychonaut', 'Python', 'quotes', 'QuotesPorn', 'ragenovels', 'Random_Acts_Of_Pizza', 'randomsexiness', 'reactiongifs', 'RealGirls', 'recipes', 'reddit.com', 'RedditThroughHistory', 'redheads', 'relationship_advice', 'relationships', 'religion', 'ReverseEngineering', 'ronpaul', 'RoomPorn', 'rpg', 'ruby', 'running', 'science', 'scifi', 'Seattle', 'secretsanta', 'seduction', 'self', 'SelfSufficiency', 'sex', 'Sexy', 'ShitRedditSays', 'shittyadvice', 'shittyaskscience', 'shutupandtakemymoney', 'skeptic', 'skyrim', 'soccer', 'socialism', 'software', 'somethingimade', 'SOPA', 'space', 'spacedicks', 'spaceporn', 'SpideyMeme', 'sports', 'StandUpComedy', 'starcraft', 'startrek', 'startups', 'StarWars', 'SubredditDrama', 'subredditoftheday', 'SuicideWatch', 'swtor', 'sysadmin', 'talesfromtechsupport', 'tattoos', 'technology', 'techsupport', 'teen_girls', 'television', 'Terraria', 'tf2', 'TheoryOfReddit', 'theredditor', 'thewalkingdead', 'thick', 'tipofmytongue', 'tldr', 'todayilearned', 'toosoon', 'TopGear', 'toronto', 'trackers', 'trance', 'travel', 'treecomics', 'trees', 'TrollXChromosomes', 'truegaming', 'TrueReddit', 'TwoXChromosomes', 'Ubuntu', 'unitedkingdom', 'UniversityofReddit', 'vegan', 'vertical', 'video', 'videos', 'VillagePorn', 'vinyl', 'voluptuous', 'wallpaper', 'wallpapers', 'waterporn', 'WeAreTheMusicMakers', 'webcomics', 'web_design', 'webdev', 'WebGames', 'wicked_edge', 'WikiLeaks', 'wikipedia', 'windows', 'windowshots', 'woahdude', 'women', 'worldnews', 'worldpolitics', 'worstof', 'wow', 'writing', 'WTF', 'xbox360', 'xkcd', 'Xsmall', 'YouShouldKnow', 'zelda', 'ZenHabits', 'zombies']
+N = 2
 
 def run():
     connection = pymongo.Connection('localhost', 27017)
     global db = connection.new_database
     r = reddit.Reddit("hackny_bot")
     r.login("bzzzz3","bzzzz")
-    for sub in SUBS_LIST:
+    f = open("subreddits.txt")
+    for sub in f.read().split():
         get_threads(sub, r)
     
 def get_reddit_id(url):
@@ -38,23 +38,22 @@ def process_comments(com):
     if 'body' in com.__dict__.keys():
         if com.body == "":
             return
-        #print com.body
-        #print
-        new_comment = {'body': com.body}
-        collection = db.comments
-        collection.insert(new_comment)
+        add_to_db(com_body)
     else:
         print (process_comments(com.comments))
 
-collection = db.comments
-collection.insert(new_comment)
-print db.collection_names()
+def add_to_db(body):
+    new_comment = {'body': body}
+    collection = db.comments
+    collection.insert(new_comment)
 
-def add_to_db():
-
+def markovify(body):
+    words = body.split()
+    for i in xrange(0,len(words)):
+        if i == len(words) - N:
+            new_markov = {'prefix':words[i]+" "+words[i+1], 'suffix': None}
+        else:    
+            new_markov = {'prefix':words[i]+" "+words[i+1], 'suffix': words[i+2]}
 
 if __name__ == "__main__":
     run()
-
-
-
