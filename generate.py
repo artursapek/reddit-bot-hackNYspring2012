86dd5d16d683f610c8919a8232c9f69024b3ceb8
jason <jason@jason-TOSHIBA-NB505.(none)>
2012-03-25 02:18:17 -0400

    fixed whate =dd

--- generate.py
+++ generate.py
@@ -1,17 +1,45 @@
 import pymongo
 import random
 
-def generate_comment(seed):
+QUESTION_WORDS = ["what",
+                  "where",
+                  "who",
+                  "would",
+                  "why",
+                  "what",
+                  "who",
+                  "how",
+                  "when",
+                  "do",
+                  "did",
+                  "would",
+                  "will",
+                  "is",
+                  "was"]
+
+def gen(seed):
+    rando = random.randint(12,20)
     comment = seed
     suff = get_suffix(seed)
-    while suff is not None:
+    while suff is not None and len(comment.split()) < rando:
+	print comment
+        print
         comment += " " + suff
-        seed = comment.split()[-2:]
+        seed = " ".join(comment.split()[-2:])
         suff = get_suffix(seed)
-    return comment
+    return prettify(comment)
+    
 
 def get_suffix(prefix):
     connection = pymongo.Connection('localhost', 27017)
     db = connection.new_database
     collection = db.comments
-    return random.choice(collection.find({prefix: prefix}))
+    suffs = collection.find({"prefix": prefix}).distinct("suffix")
+    if len(suffs) == 0:
+        return None
+    return random.choice(suffs)
+
+def prettify(comment):
+    if comment.split()[0].lower() in QUESTION_WORDS:
+        comment += "?"
+    return comment[0].capitalize()[0] + comment[1:].lower()
